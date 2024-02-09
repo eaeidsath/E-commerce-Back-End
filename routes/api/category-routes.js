5b08929a05d778096d135545085bdbc1ba2d2fa0
EE <evelyneidsath@gmail.com>
2024-02-09 02:14:57 -0800

    create routes for product-routes, fix error in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // finds one category by its `id` value with its associated Products
   try {
-    const categoryData = await Location.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
